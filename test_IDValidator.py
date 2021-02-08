5ac16734d83dfecfb2fc797a5f4244775a669694
Orsan Awawdi <orsan.awawdi@gmail.com>
2021-02-08 11:42:18 +0200

    git commit

--- test_IDValidator.py
+++ test_IDValidator.py
@@ -1,13 +1,13 @@
 def validity_check(id_number):
-
     list_of_IDs = list(map(int, str(id_number)))
 
     list_of_IDs[1::2] = map(lambda x: x * 2, list_of_IDs[1::2])
     list_of_IDs = map(lambda x: (x % 10 + x // 10), list_of_IDs)
     num1 = sum(list_of_IDs)
+    if num1 % 10 != 0:
+        return "BAD ID NUMBER"
+    else:
+        return True
 
-    assert num1 % 10 == 0 ,"must be zero" 
-
-if __name__ == "__main__":
-    validity_check("123456873")
-    print("Everything passed")
+def test_Validator():
+    assert validity_check('123456873') == True
